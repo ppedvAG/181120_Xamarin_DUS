9927453cda2d90c498524fa3ba2a84beee44afef
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-11-20 16:56:21 +0100

    v1 - ArrayAdapter

--- GetToDoListe/MainActivity.cs
+++ GetToDoListe/MainActivity.cs
@@ -3,17 +3,44 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System.Net;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace GetToDoListe
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private Button buttonToDo;
+        private ListView listViewToDo;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            buttonToDo = FindViewById<Button>(Resource.Id.buttonGetToDoListe);
+            listViewToDo = FindViewById<ListView>(Resource.Id.listViewToDo);
+
+            buttonToDo.Click += ButtonToDo_Click;
+        }
+
+        private void ButtonToDo_Click(object sender, System.EventArgs e)
+        {
+            string json;
+            using (WebClient client = new WebClient())
+            {
+                json = client.DownloadString("https://jsonplaceholder.typicode.com/todos");
+            }
+
+            ToDoItem[] items = JsonConvert.DeserializeObject<ToDoItem[]>(json);
+
+            string[] titles = items.Select(x => x.Title).ToArray();
+
+
+            listViewToDo.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, titles);
         }
     }
 }
---
+++ GetToDoListe/ToDoItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+
+namespace GetToDoListe
+{
+    public partial class ToDoItem
+    {
+        [JsonProperty("userId")]
+        public long UserId { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("completed")]
+        public bool Completed { get; set; }
+    }
+}
