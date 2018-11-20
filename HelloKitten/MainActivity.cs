2c001ab2352e7cafcba09a6307b28bea202fbe20
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-11-20 16:16:29 +0100

    Init GetToDoListe

--- HelloKitten/MainActivity.cs
+++ HelloKitten/MainActivity.cs
@@ -3,6 +3,9 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System;
+using System.Net;
+using Android.Graphics;
 
 namespace HelloKitten
 {
@@ -31,7 +34,36 @@ namespace HelloKitten
         private void ButtonKätzchenLaden_Click(object sender, System.EventArgs e)
         {
             // Variante 1) Hello World
-            Toast.MakeText(this, "Hello World", ToastLength.Short).Show(); 
+            // Toast.MakeText(this, "Hello World", ToastLength.Short).Show(); 
+
+            // 1) Bild von placekitten.com herunterladen
+
+            if(string.IsNullOrWhiteSpace(editTextHöhe.Text)|| string.IsNullOrWhiteSpace(editTextBreite.Text))
+            {
+                Toast.MakeText(this, "Bitte geben Sie die Höhe UND die Breite ein", ToastLength.Long).Show(); 
+                return;
+            }
+
+            int höhe = Convert.ToInt32(editTextHöhe.Text);
+            int breite = Convert.ToInt32(editTextBreite.Text);
+
+            if (höhe <=0 || breite <= 0)
+            {
+                Toast.MakeText(this, "Höhe oder Breite ist ungültig", ToastLength.Long).Show();
+                return;
+            }
+
+            // text wird automatisch als url angezeigt und eingefärbt -> "Enable single-click URL-Navigation"
+            byte[] imageData;
+            using (WebClient client = new WebClient())
+            {
+                imageData = client.DownloadData($"http://placekitten.com/g/{breite}/{höhe}");
+            }
+
+            // byte[] zu einer Bitmap machen und anzeigen
+
+            imageViewKätzchen.SetImageBitmap(BitmapFactory.DecodeByteArray(imageData, 0, imageData.Length));
+
         }
     }
 }
