2d83618d63528cc3185f2451eedb728003312f49
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-11-22 12:08:15 +0100

    Steuerelemente und  TableView

---
+++ Steuerelemente/Steuerelemente/EmbeddedImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Steuerelemente
+{
+    class EmbeddedImage : IMarkupExtension
+    {
+        public string ImageID { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrWhiteSpace(ImageID))
+                return null;
+
+            return ImageSource.FromResource(ImageID);
+        }
+    }
+}
--- Steuerelemente/Steuerelemente/MainPage.xaml.cs
+++ Steuerelemente/Steuerelemente/MainPage.xaml.cs
@@ -7,11 +7,43 @@ using Xamarin.Forms;
 
 namespace Steuerelemente
 {
+
+    public enum Wochentage { Montag,Dienstag,Mittwoch,Donnerstag,Freitag,Samstag,Sonntag}
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //UriImageSource source = new UriImageSource();
+            //source.Uri = new Uri("....");
+            //source.CachingEnabled = true; // Standardfall: true
+            //source.CacheValidity = TimeSpan.FromMinutes(10); // Standardfall: 24h
+
+
+            // <Image x:Name="imageBild"/>
+            // imageBild.Source = ImageSource.FromResource("Steuerelemente.Images.hamster.jpg");
+
+
+            //if(Device.Idiom == TargetIdiom.Tablet)
+            //{
+            //    if(Device.RuntimePlatform == "Bananaphone")
+            //    {
+            //        // COde für Android-Tablets
+            //    }
+            //}
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            //entryUsername.Text = e.Value.ToString();
+
+            Editor ed = new Editor();
+
+            StackLayout sl = new StackLayout();
         }
     }
 }
